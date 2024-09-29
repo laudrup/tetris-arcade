298e073ec430cc0aba367edb9573b070147a259d
Kasper Laudrup <laudrup@stacktrace.dk>
2024-09-29 13:27:58 +0200

    Make tetris-arcade a Python module
    To prepare for distribution requirements make the game implementation
    a python module

--- tetris_arcade/tetris.py
+++ tetris_arcade/tetris.py
@@ -1,12 +1,11 @@
 """
-Tetris
+Tetris-Arcade
 
-Tetris clone, with some ideas from silvasur's code:
-https://gist.github.com/silvasur/565419/d9de6a84e7da000797ac681976442073045c74a4
+Tetris clone based on the tetris example from the python arcade
+distribution.
 
-If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.tetris
 """
+
 import arcade
 import random
 import pathlib
